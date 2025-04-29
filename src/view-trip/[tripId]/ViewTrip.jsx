0ab866223c21405a5a5301f50bc2ecd63e9667b1
Pradyumn gupta <125115115+pradumn2000@users.noreply.github.com>
2025-04-29 13:37:04 +0530

    Update ViewTrip.jsx

--- src/view-trip/[tripId]/ViewTrip.jsx
+++ src/view-trip/[tripId]/ViewTrip.jsx
@@ -4,7 +4,7 @@ import { doc, getDoc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "sonner";
-import InfoSection from "./components/infoSection";
+import InfoSection from "./components/InfoSection";
 import Hotels from "./components/Hotels";
 import PlacesToVisit from "./components/PlacesToVisit";
 import Footer from "./components/Footer";
